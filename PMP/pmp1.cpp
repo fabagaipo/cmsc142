7c66ea5a0a01c0aff05f253e00d10da86e2b1f27
F <58926653+fabagaipo@users.noreply.github.com>
2021-09-23 14:40:21 +0800

    Update pmp1.cpp

--- PMP/pmp1.cpp
+++ PMP/pmp1.cpp
@@ -1,1 +1,40 @@
+#include <fstream>
+#include <iostream>
+#include <sstream>
 
+using namespace std;
+
+int main(){
+    string fileName;
+
+    cin>>fileName;
+
+    readInclude(fileName);
+
+    return 0;
+}
+
+void readInclude(string fileName){
+    ifstream reader(fileName);
+
+    if(reader.is_open()){
+        string line;
+        
+        while(getline(reader,line)){
+
+            stringstream ss(line);
+
+            string str;
+
+            ss >> str;
+
+            if(str.compare("#include") == 0){
+                ss >> str;
+                //"sample.h"
+                string f = str.substr(1,str.length()-2);
+                readInclude(f);
+            }
+            cout<<line<<end1;
+        }
+    }
+}
